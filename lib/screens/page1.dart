5ec2ede8b522b61a81ab0b24af91a6fd872fb611
NirmalScaria <alreadynirmalised@gmail.com>
2021-10-20 11:35:55 +0530

    -a

--- lib/screens/page1.dart
+++ lib/screens/page1.dart
@@ -20,25 +20,45 @@ class _Page1State extends State<Page1> {
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
           onTap: () {
-            setState(() {
-              currentIndex = 1;
-            });
-            Navigator.of(context).pushNamed(Page2.routeName);
+            Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()));
           },
           child: Container(
             padding: EdgeInsets.all(20),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Text('Go to the cat page'),
+                Text('Say my name'),
                 SizedBox(height: 20),
                 Hero(
-                    tag: 'catimage',
-                    child: Image(
-                        image: AssetImage('lib/images/cat.jpg'), height: 100))
+                  
+                      tag: 'catimage',
+                      child : ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image(
+                          image: AssetImage('lib/images/cat.jpg'), height: 100)),
+                ),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catenberg"),
+      ),
+      body: Center(
+        child: Hero(
+          tag: 'catimage',
+          child: Image(image: AssetImage('lib/images/cat.jpg'),),
         ),
       ),
     );
