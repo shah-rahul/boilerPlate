b68b88a6649e6b01a6238c784507bb8933b7518f
Rahul shah <44189476+shah-rahul@users.noreply.github.com>
2021-10-20 09:52:51 +0530

    [BUG FIX] - route for hero animation

--- lib/screens/page1.dart
+++ lib/screens/page1.dart
@@ -1,6 +1,7 @@
+import 'package:boilerplate/screens/page2.dart';
 import 'package:flutter/material.dart';
 import 'homePage.dart';
-import 'homePage.dart';
+
 class Page1 extends StatefulWidget {
   Page1({Key? key}) : super(key: key);
   static const String routeName = "/Page1";
@@ -12,7 +13,7 @@ class Page1 extends StatefulWidget {
 class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
-    final args=ModalRoute.of(context)!.settings.arguments;
+    final args = ModalRoute.of(context)!.settings.arguments;
     print(args);
     return Center(
       child: Card(
@@ -20,17 +21,21 @@ class _Page1State extends State<Page1> {
           splashColor: Colors.blue.withAlpha(30),
           onTap: () {
             setState(() {
-              currentIndex=1;
-                        });
+              currentIndex = 1;
+            });
+            Navigator.of(context).pushNamed(Page2.routeName);
           },
           child: Container(
-            padding:EdgeInsets.all(20) ,
+            padding: EdgeInsets.all(20),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Text('Go to the cat page'),
-                SizedBox(height:20),
-                Hero( tag: 'catimage' ,child: Image(image: AssetImage('lib/images/cat.jpg'), height:100))
+                SizedBox(height: 20),
+                Hero(
+                    tag: 'catimage',
+                    child: Image(
+                        image: AssetImage('lib/images/cat.jpg'), height: 100))
               ],
             ),
           ),
